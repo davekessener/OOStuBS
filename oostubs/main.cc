82d699bbd1dde98b32f3499189b651c0cf472e70
Daniel Kessener <davekessener@gmail.com>
2019-11-06 18:15:32 +0000

    added base

---
+++ oostubs/main.cc
@@ -0,0 +1,14 @@
+/* $Id: main.cc 8485 2017-03-27 11:50:06Z friesel $ */
+
+/* Hier muesst ihr selbst Code vervollstaendigen */ 
+        
+int main()
+{
+/* Hier muesst ihr selbst Code vervollstaendigen */         
+         
+/* Hier muesst ihr selbst Code vervollstaendigen */ 
+                         
+/* Hier muesst ihr selbst Code vervollstaendigen */                         
+ 
+   return 0;
+ }
